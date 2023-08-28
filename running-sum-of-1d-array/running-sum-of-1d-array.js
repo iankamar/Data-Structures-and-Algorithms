552490910e9e8f0fee0853ef4dda461a7b5a3112
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-08-28 00:58:06 -0400

    Time: 57 ms, Memory: 42.8 MB - LeetHub

--- running-sum-of-1d-array/running-sum-of-1d-array.js
+++ running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -2,18 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-/*
-var runningSum = function(nums) {
-  let sum = 0;
-    for (let i = 0; i< nums.lengt; i++) {
-        sum += nums[i];
-        nums[i]=s;
-    }
-    return nums
-};
-*/
 
+var runningSum = function(nums) {
+    nums.reduce((a,c,i,arr) => arr[i] += a)
+    return nums
+}
+
+/*
 const runningSum = nums => {
     nums.reduce((a,c,i,arr) => arr[i] += a)
     return nums
 }
+*/
