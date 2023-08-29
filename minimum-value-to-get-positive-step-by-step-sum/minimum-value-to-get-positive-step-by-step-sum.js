e64429da0e3f9a762189d210b717a0155f0a99d2
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-08-29 02:49:45 -0400

    Time: 50 ms, Memory: 41.7 MB - LeetHub

--- minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
+++ minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -10,12 +10,3 @@ var minStartValue = function(nums) {
     })
     return min >=1 ? 1: Math.abs(min -1);
 };
-/*
-function minStartValue(nums) {
-  let min = Infinity;
-  let sum = 0;
-  nums.forEach(n => {
-    min = Math.min(min, sum += n);
-  });
-  return min >= 1 ? 1 : Math.abs(min - 1);
-} */
