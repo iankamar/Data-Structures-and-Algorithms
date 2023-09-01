f3250158308826835fb33b78f44ffcc18c30ee15
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-01 17:18:56 -0400

    Time: 514 ms, Memory: 33.5 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        my_dict = {}
+        for i in nums:
+            if i in my_dict:
+                return True
+            else:
+                my_dict[i] = 1
+            if len(my_dict) == len(nums):
+                return False
+    
+    
