30595773e8792bd06e1a55169eb1ea78e2e1f32c
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-05 23:56:30 -0400

    Time: 54 ms, Memory: 42.2 MB - LeetHub

---
+++ plus-one/plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+var plusOne = function(digits) {
+    let end = digits.length - 1;
+    for(let i = end; i >= 0; i--){
+        if(digits[i] !== 9){
+            digits[i] = digits[i] + 1;
+            break;
+        } else {
+            digits[i] = 0;
+        }
+    }
+
+    if(digits[0] === 0) digits.unshift(1);
+    return digits;
+};
