0e1fd3c9d798d216e356d191e318ff55a4b2e886
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-15 00:55:27 -0400

    Time: 84 ms, Memory: 47.1 MB - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let maxLength = 0;
+    let set = new Set();
+    let i = 0;
+    let j = 0;
+    while (i < s.length && j < s.length) {
+        if (!set.has(s[j])) {
+            set.add(s[j]);
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
+        } else {
+            set.delete(s[i]);
+            i++;
+        }
+    }
+    return maxLength;
+};
+
