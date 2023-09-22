9920550230d7ead2d8c3dc88e54eca9f6859432f
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-22 16:26:19 -0400

    Time: 50 ms, Memory: 41.3 MB - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+var strStr = function(haystack, needle) {
+    if(haystack.match() != null){
+        return haystack.indexOf(needle);
+    }
+    
+    return -1;
+};
