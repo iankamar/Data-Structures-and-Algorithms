9c8b8796cba9691a6b2e678cadd6db105d5b40f6
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-25 15:53:17 -0400

    Time: 67 ms, Memory: 46.2 MB - LeetHub

--- valid-sudoku/valid-sudoku.js
+++ valid-sudoku/valid-sudoku.js
@@ -3,24 +3,24 @@
  * @return {boolean}
  */
 
-var isValidSudoku = function(board) {
-  const seen = new Set()
-  for (let i = 0; i < 9; ++i) {
-    for (let j = 0; j < 9; ++j) {
-      let char = board[i][j]
-      if (char !== '.') {
-        if (
-          seen.has(`${char} in row ${i}`) ||
-          seen.has(`${char} in col ${j}`) ||
-          seen.has(`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)
-        ) {
-          return false
+var isValidSudoku  = function (board) {
+    const seen = new Set()
+    for (let i = 0; i < 9; ++i) {
+        for (let j = 0; j < 9; ++j) {
+            let char = board[i][j]
+            if (char !== '.') {
+                if (
+                seen.has (`${char} in row ${i}`) ||
+                seen.has (`${char} in col ${j}`) ||
+                seen.has (`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)
+                    ) {
+                    return false
+                }
+                seen.add(`${char} in row ${i}`)
+                seen.add(`${char} in col ${j}`)
+                seen.add(`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)
+            }
         }
-        seen.add(`${char} in row ${i}`)
-        seen.add(`${char} in col ${j}`)
-        seen.add(`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)
-      }
     }
-  }
-  return true
+    return true
 };
