472558d3f4d9f1843c008e92cb1debe9424e66fe
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-09-29 01:23:33 -0400

    Time: 46 ms, Memory: 16.3 MB - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        head = tail= ListNode(10) # any value could be used as the comparison is between l1 and l2
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+            
+        tail.next = l1 or l2
+        return head.next  # here by using pointer to next you only pointing to the merged list after the initated random value of 10 that should be excluded
