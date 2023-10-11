e9cb3dff2c51721bcc287997245ee2d9ea8bb33e
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-10-11 14:02:38 -0400

    Time: 367 ms, Memory: 68.8 MB - LeetHub

---
+++ power-of-three/power-of-three.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isPowerOfThree = function(n) {
+    if(n===0){
+        return false;
+    } 
+    while(n!==1){
+        val=n/3;
+        console.log(val,'val');
+        if(val%3!==0 && val!==1){
+            return false;
+        }
+        n=val;
+        console.log(n,'n');
+    }
+    return true;
+};
