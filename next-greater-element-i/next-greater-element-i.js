b6c4ae34e7f1a87b6484a2afec0aa3398bc155f2
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-10-22 21:15:35 -0400

    Time: 49 ms, Memory: 43.6 MB - LeetHub

---
+++ next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+var nextGreaterElement = function(nums1, nums2) {
+  const res = []
+  for(let n of nums1){
+    const idx = nums2.indexOf(n)
+    let c = idx+1, found = -1
+    while(nums2[idx] > nums2[c])c++
+    if(c<nums2.length) found = nums2[c]
+    res.push(found)
+  }
+  return res
+};
