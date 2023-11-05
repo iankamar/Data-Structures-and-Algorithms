d357b508024580a6b190ebf5e1999f52041075b8
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-11-04 23:14:46 -0400

    Time: 46 ms, Memory: 42.4 MB - LeetHub

---
+++ minimum-genetic-mutation/minimum-genetic-mutation.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function(start, end, bank) {
+    let tempBanks = new Set(bank);  
+    let strChars = ['A', 'T', 'C', 'G'];        
+    let Q = [[start,0]];
+    let item, dist, i, j;
+    if( !tempBanks.has(end) ) return -1;
+    
+    while( Q.length > 0 ) {
+        [item,dist] = Q.shift();
+        if( item == end ) return dist;
+        
+        for( i=0; i<8; i++ ) {
+            for( j=0; j<4; j++ ) {
+                if( item[i] == strChars[j] ) continue;
+                var node = item.slice(0,i) + strChars[j] + item.slice(i+1);
+                if( tempBanks.has(node) ) {
+                    Q.push([node,dist+1]);
+                    tempBanks.delete(node);
+                }
+            }
+        }
+    }   
+    return -1;
+};
