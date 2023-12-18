e8fbc713132557fec97120fed5e71c9e852e7d28
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-12-17 20:04:47 -0500

    Time: 51 ms, Memory: 42 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.js
@@ -0,0 +1,15 @@
+var thirdMax = function(nums) {
+  let len = nums.length;
+  
+  nums.sort((a, b) => b - a);
+  
+  let count = 0;
+  for (let i = 0; i < len; i++) {
+    if (!nums[i+1] || nums[i] !== nums[i+1]) {
+      count++;
+      if (count === 3) return nums[i];
+    }
+  }
+
+  return nums[0];
+};
