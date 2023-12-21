e6bc485311c0f7d0001270802234ba25a496a73a
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2023-12-20 23:55:15 -0500

    Time: 55 ms, Memory: 42 MB - LeetHub

--- height-checker/height-checker.js
+++ height-checker/height-checker.js
@@ -1,6 +1,6 @@
-var heightChecker = function(heights) {
-    let etalon = [...heights].sort((a,b)=>a-b);
-    let count = 0;
-    etalon.forEach((a,b)=>heights[b] !== etalon[b] ? count++ : 0);
-    return count;
+const heightChecker = heights => {
+    const heightsClone = [...heights];
+    heights.sort((a,b) => a - b);
+    
+    return heightsClone.reduce((acc, val, i) => acc + !(heights[i] === val), 0);
 };
