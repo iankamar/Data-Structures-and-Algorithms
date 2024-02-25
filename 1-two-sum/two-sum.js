b184cbdfa133351207eb87d4a736a642ec6f12e4
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2024-02-25 04:35:44 -0500

    Time: 60 ms (71.10%) | Memory: 50.9 MB (25.81%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -1,12 +1,22 @@
-function twoSum(nums, target) {
-    const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(nums[i], i);
-    }
-    return [];
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 
+
+var twoSum = function(nums, target) {
+    let numIndices = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        
+        if (numIndices.has(complement)) {
+            return [numIndices.get(complement), i];
+        }
+        
+        numIndices.set(nums[i], i);
+    }
+    
+    return [];
+};
