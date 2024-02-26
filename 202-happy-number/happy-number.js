e16da28d1deebf70b56ada689e35df79a276f2be
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2024-02-26 02:16:00 -0500

    Time: 63 ms (38.88%) | Memory: 51.8 MB (8.21%) - LeetSync

---
+++ 202-happy-number/happy-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    var seen = {};
+    while (n !== 1 && !seen[n]) {
+        seen[n] = true;
+        n = sumOfSquares(n);
+    }
+    return n === 1;
+};
+
+function sumOfSquares(numString) {
+    return numString.toString().split('').reduce(function(sum, num) {
+        return sum + Math.pow(num, 2);
+    }, 0);
+}
