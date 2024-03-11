fc4d83e110fa9176f3e6181ab0e2e1c400218fdc
Ian Kamar <95672055+iankamar@users.noreply.github.com>
2024-03-11 13:41:50 -0400

    Time: 79 ms (43.41%) | Memory: 52.9 MB (24.50%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x < 2) return x;
+    let left = 1;
+    let right = Math.floor(x / 2);
+
+    while (left <= right) {
+        let mid = left + 
+        Math.floor ((right - left) / 2);
+
+        if (mid * mid === x)
+        return mid;
+
+        if (mid * mid < x ) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return  right;
+};
